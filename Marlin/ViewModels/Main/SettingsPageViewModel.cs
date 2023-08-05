6f8b38efa15e3419442c0beb45bc83e1597bc532
Bolofficial <boloninnn@gmail.com>
2023-08-05 12:53:02 +0300

    Update

--- Marlin/ViewModels/Main/SettingsPageViewModel.cs
+++ Marlin/ViewModels/Main/SettingsPageViewModel.cs
@@ -8,37 +8,19 @@ namespace Marlin.ViewModels.Main
         public string PageColor
         {
             get => Context.Settings.Theme.PageColor;
-            set
-            {
-                if (value[0] == '#' && (value.Length == 9 || value.Length == 7))
-                {
-                    Set(ref Context.Settings.Theme.PageColor, value);
-                }
-            }
+            set => Set(ref Context.Settings.Theme.PageColor, value);
         }
 
         public string FontColor
         {
             get => Context.Settings.Theme.FontColor;
-            set
-            {
-                if (value[0] == '#' && (value.Length == 9 || value.Length == 7))
-                {
-                    Set(ref Context.Settings.Theme.FontColor, value);
-                }
-            }
+            set => Set(ref Context.Settings.Theme.FontColor, value);
         }
 
         public string BackgroundColor
         {
             get => Context.Settings.Theme.BackgroundColor;
-            set
-            {
-                if (value[0] == '#' && (value.Length == 9 || value.Length == 7))
-                {
-                    Set(ref Context.Settings.Theme.BackgroundColor, value);
-                }
-            }
+            set => Set(ref Context.Settings.Theme.BackgroundColor, value);
         }
 
         public bool IsSay
@@ -57,8 +39,8 @@ namespace Marlin.ViewModels.Main
             get => Context.Settings.Speed;
             set
             {
-                Voix.SpeakAsync($"Установлена скорость озвучивания {value}");
                 Set(ref Context.Settings.Speed, value);
+                Voix.SpeakAsync($"Установлена скорость озвучивания {value}");
             }
         }
 
